93d33eb249ae6f19cca1f02885566305543fa0ab
robbiemurphy <rmurphy987@gmail.com>
2022-01-09 02:44:06 -0600

    constants and window prompts added

--- develop/script.js
+++ develop/script.js
@@ -1,17 +1,41 @@
-// Assignment code here
+// CONSTANTS
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '~`! @#$%^&*()_-+={[}]|';
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// #generate ELEMENT
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// EventListener
+generateBtn.addEventListener('click', writePassword);
+
+    function writePassword() {
+        var promptPasswordLength = window.prompt('Your password must be between 8 and 128 characters. Write a number of your choosing below that coincides with the aformentioned integer requirment.');
+
+        //while (promptPasswordLength > 8 && > 128) {
+        //    var confirmNum = window.confirm('Please write a number between 8 and 128.');
+        //}
+
+        var promptLowerCase = window.confirm('Should your password include lowercase letters?');
+
+        var promptUpperCase = window.confirm('Should your password include uppercase letters?');
+
+        var promptNumeric = window.confirm('Should your password include numbers?');
+
+        var promptSymbols = window.confirm('Should your password include symbols?');
+    }
+
+    //function writePassword() {
+    //    var promptIntegerLength = window.confirm('Select ')
+    // }
